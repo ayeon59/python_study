7214a23e88b161f21f15cbf09bbef19ff200f6b1
A Yeon Jang <125449701+ayeon59@users.noreply.github.com>
2025-04-28 13:31:03 +0900

    ch_07_answer

--- ch07_answer/ch07_순열구하기.py
+++ ch07_answer/ch07_순열구하기.py
@@ -8,15 +8,15 @@ def DFS(L):
         return
     else:
         for i in range(1,n+1):
-            if i in res[:L]:  # 이미 사용된 숫자면 제외
+            if i in res[:L]:  
                 continue
             res[L] = i
             DFS(L+1)
 
-
+ㄴ
 
 n,m = map(int,input().split())
-res = [0]*m
+res = [0]ㄴ*m
 cnt = 0
 DFS(0)
 print(cnt)
