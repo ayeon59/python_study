a97693df43117e018cb597060691bd7e23ed168d
A Yeon Jang <125449701+ayeon59@users.noreply.github.com>
2025-05-01 16:53:39 +0900

    ch07_answers

--- ch07_answer/ch07_순열구하기.py
+++ ch07_answer/ch07_순열구하기.py
@@ -1,22 +1,22 @@
 def DFS(L):
-    global cnt
+    global cnt 
     if(L==m):
-        for j in range(m):
-            print(res[j], end=' ')
+        for i in range(L):
+            print(res[i], end=' ')
         print()
         cnt += 1
-        return
-    else:
+    else :
         for i in range(1,n+1):
-            if i in res[:L]:  
-                continue
-            res[L] = i
-            DFS(L+1)
+            if ch[i]==0:
+                res[L]=i
+                ch[i]=1
+                DFS(L+1)
+                ch[i]=0
 
-ㄴ
 
 n,m = map(int,input().split())
-res = [0]ㄴ*m
+res = [0]*m
+ch = [0]*(n+1)
 cnt = 0
 DFS(0)
 print(cnt)
