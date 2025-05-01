0e387521477d1fc315eaa50834283adce5ab94cf
A Yeon Jang <125449701+ayeon59@users.noreply.github.com>
2025-05-02 00:33:22 +0900

    Create ch08_양팔저울.py

---
+++ ch08_answer/ch08_양팔저울.py
@@ -0,0 +1,18 @@
+def DFS(L):
+    global cnt
+    if(L==limit):
+        print(cnt)
+    else:
+        for i in range(k):
+            if(plat_1[i])
+
+
+        
+
+k=int(input())
+plat_1=[0]*(k)
+plat_2=[0]*(k)
+cnt=0
+kg=list(map(int,input().split()))
+limit=sum(kg)
+DFS(0)
